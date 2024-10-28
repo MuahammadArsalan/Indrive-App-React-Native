0383c64fa3848a74c6ab94cd233d12b966f4b752
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-10-29 00:00:41 +0500

    In Progress

--- app/index.tsx
+++ app/index.tsx
@@ -118,6 +118,7 @@ const searchPlaces =  ()=>{
         }}
         keyExtractor={(item:{fsq_id:string}) => {item.fsq_id}}      />}
 {/* map */}
+
       <MapView
         style={styles.map}
         initialRegion={{
